f86bfce674d8b9741041aa7cc57aa3802bcb1945
徐瑞骏 <1687701765@qq.com>
2025-06-19 23:11:54 +0800

    将SPOT元数据读取的函数封装成`SPOTMetadata`类，添加`read_calib`方法

--- spot_metadata_gen_rpc.py
+++ spot_metadata_gen_rpc.py
@@ -20,42 +20,56 @@ import sys
 import xml.etree.ElementTree as ET
 import numpy as np
 
+class SPOTMetadata:
+    def __init__(self, path):
+        self.path = path
+        self.tree = ET.parse(path)
 
-def get_floats(node):
-    return tuple(map(lambda x:float(x.text), node))
+    # ref: *The SPOT Scene Standard Digital Product Format* p.57
+    # on https://regards.cnes.fr/user/swh/modules/54  Identifier:S4-ST-73-01-SI
+    def read_spot_rpc_simplified(self):
+        def get_floats(node):
+            return tuple(map(lambda x:float(x.text), node))
+        def get_lcpc(model):
+            lc = get_floats(model.find('lc_List'))
+            pc = get_floats(model.find('pc_List'))
+            return lc, pc
+        root = self.tree.getroot()
+        center = get_floats(root.find('Dataset_Frame/Scene_Center'))[:2]
+        imgsize = get_floats(root.find('Raster_Dimensions'))[:2]
+        dire = get_lcpc(root.find('Geoposition/Simplified_Location_Model/Direct_Location_Model'))
+        reve = get_lcpc(root.find('Geoposition/Simplified_Location_Model/Reverse_Location_Model'))
+        return center, imgsize, dire, reve
 
-def get_lcpc(model):
-    lc = get_floats(model.find('lc_List'))
-    pc = get_floats(model.find('pc_List'))
-    return lc, pc
+    def read_spot1_4_rpc(self):
+        def get_floats(node):
+            return tuple(map(lambda x:float(x.text), node))
+        root = self.tree.getroot()
+        obm = root.find('Data_Strip/Models/OneB_Model')
+        if obm is None:  # 我下载的SPOT5元数据里没有OneB_Model
+            return
+        Norm = get_floats(obm.find('Coordinate_Normalization'))
+        A = get_floats(obm.find('A'))
+        B = get_floats(obm.find('B'))
+        I = get_floats(obm.find('Direct_OneB_Model/I'))
+        J = get_floats(obm.find('Direct_OneB_Model/J'))
+        L = get_floats(obm.find('Reverse_OneB_Model/L'))
+        P = get_floats(obm.find('Reverse_OneB_Model/P'))
+        # TODO: 使用精度更高的模型
 
-# ref: *The SPOT Scene Standard Digital Product Format* p.57
-# on https://regards.cnes.fr/user/swh/modules/54  Identifier:S4-ST-73-01-SI
-def read_spot_rpc_simplified(metadata_path):
-    tree = ET.parse(metadata_path)
-    root = tree.getroot()
-    center = get_floats(root.find('Dataset_Frame/Scene_Center'))[:2]
-    imgsize = get_floats(root.find('Raster_Dimensions'))[:2]
-    dire = get_lcpc(root.find('Geoposition/Simplified_Location_Model/Direct_Location_Model'))
-    reve = get_lcpc(root.find('Geoposition/Simplified_Location_Model/Reverse_Location_Model'))
-    return center, imgsize, dire, reve
+    def read_calib(self):
+        root = self.tree.getroot()
+        band_params = list(root.find('Data_Strip/Sensor_Calibration/Calibration'))
+        band_lst = []
+        for band in band_params:
+            cell_lst = []
+            for cell in band.find('Gain_Section/Pixel_Parameters/Cells'):
+                G = float(cell.find('G').text)
+                DC = float(cell.find('DARK_CURRENT').text)
+                cell_lst.append((G, DC))
+            band_lst.append(cell_lst)
+        return band_lst
 
-def read_spot1_4_rpc(metadata_path):
-    def get_floats(node):
-        return tuple(map(lambda x:float(x.text), node))
-    tree = ET.parse(metadata_path)
-    root = tree.getroot()
-    obm = root.find('Data_Strip/Models/OneB_Model')
-    if obm is None:  # 我下载的SPOT5元数据里没有OneB_Model
-        return
-    Norm = get_floats(obm.find('Coordinate_Normalization'))
-    A = get_floats(obm.find('A'))
-    B = get_floats(obm.find('B'))
-    I = get_floats(obm.find('Direct_OneB_Model/I'))
-    J = get_floats(obm.find('Direct_OneB_Model/J'))
-    L = get_floats(obm.find('Reverse_OneB_Model/L'))
-    P = get_floats(obm.find('Reverse_OneB_Model/P'))
-    # TODO: 使用精度更高的模型
 
 # ref: https://gdal.org/en/stable/drivers/raster/gtiff.html#georeferencing
 # ref: http://geotiff.maptools.org/rpc_prop.html
@@ -113,7 +127,8 @@ def write_rpc(fn, center, imgsize, model_i, model_j):
 if __name__ == '__main__':
     path_in = sys.argv[1]
     path_out = sys.argv[2]
-    center, imgsize, _, (model_i, model_j) = read_spot_rpc_simplified(path_in)
+    metadata = SPOTMetadata(path_in)
+    center, imgsize, _, (model_i, model_j) = metadata.read_spot_rpc_simplified()
     write_rpc(sys.argv[2], center, imgsize, model_i, model_j)
     # RPC文件应该是 影像文件名+_RPC.TXT TODO: 自动决定输出文件名
     # 生成RPC后可用gdalwarp进行变换，如:
